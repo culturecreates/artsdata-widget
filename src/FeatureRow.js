87bf250019763ccd54c311dfcc49c0f88670167c
Gregory Saumier-Finch <gregory@culturecreates.com>
2022-09-25 11:43:56 -0500

    removed useless constructor

--- src/FeatureRow.js
+++ src/FeatureRow.js
@@ -1,29 +1,18 @@
-
-import React from 'react';
+import React from "react";
 
 export default class FeatureRow extends React.Component {
-   constructor() {
-      super();
-     
-   }
+  nameCell() {
+    let parts = [<span key="name">{this.props.datasetLabel}</span>];
+    return parts;
+  }
 
-
-   nameCell() {
-      let parts = [
-        <span key='name'>{this.props.datasetLabel}</span>
-      ];
-    
-      return parts;
-   }
-
-
-   render() {
-      return (
-        <tr>
-          <td>{this.nameCell()}</td>   
-            <td>{this.props.typeLabel}</td>
-            <td>{this.props.count}</td>
-        </tr>);
-   }
+  render() {
+    return (
+      <tr>
+        <td>{this.nameCell()}</td>
+        <td>{this.props.typeLabel}</td>
+        <td>{this.props.count}</td>
+      </tr>
+    );
+  }
 }
-
