dd331173a6c990601f89478f277188528abfbd5e
Gregory Saumier-Finch <gregory@culturecreates.com>
2022-09-24 22:59:46 -0500

    Updates

---
+++ precache-manifest.6d2db8447de16da261cbde8922e5fb43.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22f441db3691fe8a2ab478fc0c83ff82",
+    "url": "/artsdata-widget/index.html"
+  },
+  {
+    "revision": "1e00fa8d940706378801",
+    "url": "/artsdata-widget/static/css/2.2085ef74.chunk.css"
+  },
+  {
+    "revision": "a8011c14a5695757f3b0",
+    "url": "/artsdata-widget/static/css/main.dd0b5305.chunk.css"
+  },
+  {
+    "revision": "1e00fa8d940706378801",
+    "url": "/artsdata-widget/static/js/2.efe93bc6.chunk.js"
+  },
+  {
+    "revision": "a8011c14a5695757f3b0",
+    "url": "/artsdata-widget/static/js/main.89c1cb37.chunk.js"
+  },
+  {
+    "revision": "adbe88ce166af0b09863",
+    "url": "/artsdata-widget/static/js/runtime~main.87bf1406.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/artsdata-widget/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/artsdata-widget/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/artsdata-widget/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/artsdata-widget/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/artsdata-widget/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
